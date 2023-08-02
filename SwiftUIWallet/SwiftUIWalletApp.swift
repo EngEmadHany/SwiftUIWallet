8b195f064091935912da6624b744a9e9dd30a58d
EngEmadHany <emadhany2007@gmail.com>
2023-08-03 00:05:45 +0400

    Initial Commit

---
+++ SwiftUIWallet/SwiftUIWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWalletApp.swift
+//  SwiftUIWallet
+//
+//  Created by Emad Isaac on 5/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            WalletView()
+        }
+    }
+}
